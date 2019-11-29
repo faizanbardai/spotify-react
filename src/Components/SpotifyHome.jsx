f78677bfb8782ed9b87e09504421b419dafa9da3
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-11-29 10:02:06 +0100

    Linked Spotify Home Page to index.js

---
+++ src/Components/SpotifyHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SpotifyHome extends Component {
+    render() {
+        return (
+            <div>
+                Spotify Home
+            </div>
+        );
+    }
+}
+
+export default SpotifyHome;
