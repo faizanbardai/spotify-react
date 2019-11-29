d06a88ad581f442139aeffbbaafcf8f0a48b93cf
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-11-29 10:12:24 +0100

    Bootstrap, Nav with search and route

--- src/Components/SpotifyHome.jsx
+++ src/Components/SpotifyHome.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from './NavBar';
 
 class SpotifyHome extends Component {
     render() {
         return (
-            <div>
-                Spotify Home
-            </div>
+            <header className="container-fluid">
+                <Router>
+                    <NavBar />
+                    
+                </Router>
+            </header>
         );
     }
 }
